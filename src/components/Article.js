dc8f95d38a8f7a6ed1f8e19a95eca70bb4be8a8b
Ahmed Chawshin <ahmed.n.chawshin@hiof.no>
2022-04-23 03:22:05 +0200

    fixed caps

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,6 +1,6 @@
 import screenSize from "../lib/design/screensize";
 import redirect from "../lib/web/redirect";
-import ThumbnailBtn from "./articleBtn";
+import ThumbnailBtn from "./ArticleBtn";
 
 const Thumbnail = ({ imageUrl, title, id, url, data }) => {
   return (
