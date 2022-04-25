8f14545941295a60ab2c3a9a6389f3216eadbe44
Ahmed Chawshin <ahmed.n.chawshin@hiof.no>
2022-04-25 14:21:59 +0200

    added tabindex & aria

--- src/components/Article.js
+++ src/components/Article.js
@@ -10,6 +10,7 @@ const Thumbnail = ({ imageUrl, title, id, url, data }) => {
       <div className="top-row" onClick={() => redirect(url)}>
         <figure>
           <img
+            ariaLabel={`Image:  ${title}`}
             src={
               screenSize(400)
                 ? `${imageUrl}&height=150&width=150`
@@ -17,15 +18,16 @@ const Thumbnail = ({ imageUrl, title, id, url, data }) => {
             }
             alt={title}
             title={title}
-            id="imgArticle"
+            id={`imgArticle ${id}`}
           />
         </figure>
       </div>
 
-      <span className={`title ${id}`} onClick={() => redirect(url)}>
+      <h3 className={`title ${id}`} tabindex={id} ariaLabel={title}  onClick={() => redirect(url)}>
         {title}
-      </span>
-      <ArticleBtn id={id} title={title} data={data} />
+      </h3>
+      
+      <ArticleBtn id={id} title={title} ariaLabel={`Endre tittel for: ${title}`} data={data} />
     </article>
   );
 };
